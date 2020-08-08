6681204b0865ea8fc16aafa30efb4b740faddbcb
Johnnie Tirado <tiradoyamin@gmail.com>
2020-08-08 00:02:09 -0500

    First

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,7 +4,7 @@ import { ExampleComponent } from 'kamel-ui'
 import 'kamel-ui/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent text="Create React Library Example Help 😄" />
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react'
-import styles from './styles.module.css'
+import styles from './styles.module.css';
+import './index.scss';
 
 interface Props {
   text: string
 }
 
 export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  return (
+    <div>
+      <div className={styles.test}>Example Component Done: {text}</div>
+      <button>Hello</button>
+    </div>
+  )
 }
